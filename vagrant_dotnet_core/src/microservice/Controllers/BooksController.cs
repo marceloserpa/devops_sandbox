cc8c4076077d157903e36545f04159a6e9e24535
marceloserpa <jdev.marcelo@gmail.com>
2019-07-24 20:34:41 -0300

    vagrant: .net core

---
+++ vagrant_dotnet_core/src/microservice/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using microservice.Models;
+
+namespace microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            List<Book> books = new List<Book>();
+            books.Add(new Book() {Author="Stephen King", Title="The Shining"});
+            books.Add(new Book() {Author="Stephen King", Title="Under the Dome"});
+            return books;
+        }
+
+    }
+}
---
+++ vagrant_dotnet_core/src/microservice/Models/Books.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace microservice.Models
+{
+
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+    }
+}
