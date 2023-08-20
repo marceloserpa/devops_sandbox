7aa0ba764b90adb1cb5283380917fd3d1f695e19
marceloserpa <jdev.marcelo@gmail.com>
2023-08-20 20:05:17 -0300

    make bucket private and configure cloudfront

--- terraform_aws_s3cloudfront/infra-structure/main.tf
+++ terraform_aws_s3cloudfront/infra-structure/main.tf
@@ -1,7 +1,5 @@
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket-name
-  #acl = "public-read"
-  ##policy = data.aws_iam_policy_document.website_policy.json
 }
 
 resource "aws_s3_bucket_website_configuration" "website_bucket_site_config" {
@@ -30,16 +28,16 @@ resource "aws_s3_bucket_acl" "website_bucket_acl" {
   depends_on = [aws_s3_bucket_ownership_controls.website_bucket_ocs]
 
   bucket = aws_s3_bucket.website_bucket.id
-  acl    = "public-read"
+  acl    = "private"
 }
 
 resource "aws_s3_bucket_public_access_block" "website_bucket_ab" {
   bucket = aws_s3_bucket.website_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "allow_public_access" {
@@ -50,10 +48,15 @@ resource "aws_s3_bucket_policy" "allow_public_access" {
 data "aws_iam_policy_document" "allow_public_access_document" {
   statement {
     principals {
-      type        = "AWS"
-      identifiers = ["*"]
+      type = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
     }
 
+    condition {
+      test = "StringEquals"
+      variable = "AWS:SourceArn"
+      values = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
     actions = [
       "s3:GetObject"
     ]
@@ -64,5 +67,62 @@ data "aws_iam_policy_document" "allow_public_access_document" {
       aws_s3_bucket.website_bucket.arn,
       "${aws_s3_bucket.website_bucket.arn}/*",
     ]
+    
+  }
+}
+
+locals {
+  s3_origin_id = "marcelo-multisite-origin"
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "example"
+  description                       = "Example Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["BR"]
+    }
+  }
+ 
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
