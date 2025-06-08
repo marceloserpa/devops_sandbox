fd682493868505beadaa3c2caadeb519211803bc
marceloserpa <jdev.marcelo@gmail.com>
2025-06-08 14:26:01 -0300

    RabbitMQ - migrator

--- rabbitmq-migration/migrator/rabbit-migrator.py
+++ rabbitmq-migration/migrator/rabbit-migrator.py
@@ -4,6 +4,7 @@ import requests
 
 
 class BrokerInfo:
+  
   def __init__(self, host: str, port: int, user: str, password: str):
     self.host = host
     self.port = port
@@ -17,6 +18,7 @@ def get_definitions(broker: BrokerInfo):
     if resp.status_code != 200:
         print(f"[ERROR] Failed to get definitions: {resp.status_code} {resp.text}")
         sys.exit(1)
+    print(resp.text)
     return resp.json()
 
 
@@ -31,6 +33,32 @@ def import_definitions(broker: BrokerInfo, definitions: dict):
         sys.exit(1)
     print("[+] Definitions successfully imported to Amazon MQ")
 
+def normalize_bindings(bindings) -> set:
+    bindings_normalized = set()
+    for binding in bindings:
+        bindings_normalized.add(f"binding:{binding['source']}-{binding['destination']}-{binding['destination_type']}-{binding['routing_key']}")
+    return bindings_normalized
+
+def normalize_exchanges(exchanges) -> set:
+    exchanges_normalized = set()
+    for exchange in exchanges:
+        exchanges_normalized.add(f"exchange:{exchange['name']}-{exchange['type']}-{exchange['durable']}-{exchange['auto_delete']}")
+    return exchanges_normalized
+
+def normalize_queues(queues) -> set:
+    queues_normalized = set()
+    for queue in queues:
+        queues_normalized.add(f"queue:{queue['name']}-{queue['durable']}-{queue['auto_delete']}")
+    return queues_normalized
+
+
+def compare(source_definitions: dict, target_definitions: dict) -> bool:
+
+    source = normalize_bindings(source_definitions['bindings']) | normalize_exchanges(source_definitions['exchanges']) | normalize_queues(source_definitions['queues'])
+    target = normalize_bindings(target_definitions['bindings']) | normalize_exchanges(target_definitions['exchanges']) | normalize_queues(target_definitions['queues'])
+
+    return (source - target)
+
 
 def migrate(broker1: BrokerInfo, broker2: BrokerInfo):
     print("** Starting **")
@@ -43,6 +71,15 @@ def migrate(broker1: BrokerInfo, broker2: BrokerInfo):
     target['queues']    = source['queues']
 
     import_definitions(broker2, target)
+    target_updated = get_definitions(broker2)
+
+    print("## Validating Migration")
+    not_found_items = compare(source, target_updated)
+
+    print(f"{len(not_found_items)} items not found in target broker")
+    for item in not_found_items:
+        print(item)
+    print("=========================")
 
 if __name__ == "__main__":
     broker1 = BrokerInfo('localhost', 15672, 'admin', '123456')
